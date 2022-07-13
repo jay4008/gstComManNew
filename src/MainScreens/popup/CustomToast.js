1c6a760295c5a2e32e9a554ce06f23efa4bf3ac0
Jay Prakash Shah <jayprakash.s@spurtreetech.in>
2022-07-13 18:05:57 +0530

    changes

--- src/MainScreens/popup/CustomToast.js
+++ src/MainScreens/popup/CustomToast.js
@@ -1,83 +1,83 @@
-import React, { useEffect, useRef } from 'react';
-import { Animated, Dimensions, Platform, StyleSheet, Text,View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { Colors, Fonts } from '../../assets/common/common';
-import { RootState } from '../../store';
-import { setToastMsgToNUll } from '../../Store/popup';
+import React, {useEffect, useRef} from 'react';
+import {
+  Animated,
+  Dimensions,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {Colors, Fonts} from '../../assets/common/common';
+import {RootState} from '../../store';
+import {setToastMsgToNUll} from '../../Store/popup';
 // import { setToastMsgToNUll } from '../../store/mentorRegistration/MentorRegistration';
 
 const CustomToast = () => {
-    const dispatch = useDispatch();
-    const toastMsg = useSelector(
-        (state) => state.popup.message,
-    );
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    const fadeIn = () => {
-        Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 800,
-        }).start();
-    };
+  const dispatch = useDispatch();
+  const toastMsg = useSelector(state => state.popup.message);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 800,
+    }).start();
+  };
 
-    const animations = () => {
-        fadeIn();
-        setTimeout(() => {
-            fadeOut();
-            dispatch(setToastMsgToNUll());
-        }, 2000);
-    };
-    useEffect(() => {
-        if (toastMsg !== '') {
-            animations();
-        }
-    }, [toastMsg]);
-    const fadeOut = () => {
-        // Will change fadeAnim value to 0 in 3 seconds
-        Animated.timing(fadeAnim, {
-            toValue: 0,
-            duration: 400,
-            useNativeDriver:false
-
-        }).start();
-    };
-    return (
-        <View
-        
-            style={{
-                ...styles.viewStyle,
-                opacity: fadeAnim,
-                paddingHorizontal : 15,
-                top:
-                    Platform.OS === 'ios'
-                        ? toastMsg === ''
-                            ? -100
-                            : 50
-                        : toastMsg === ''
-                            ? -100
-                            : 20,
-                            
-            }}
-        >
-            <Text style={{ color: Colors.white, fontFamily: Fonts.latoBold }}>
-                {toastMsg}
-            </Text>
-
-        </View>
-    );
+  const animations = () => {
+    fadeIn();
+    setTimeout(() => {
+      fadeOut();
+      dispatch(setToastMsgToNUll());
+    }, 2000);
+  };
+  useEffect(() => {
+    if (toastMsg !== '') {
+      animations();
+    }
+  }, [toastMsg]);
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
+  };
+  return (
+    <Animated.View
+      style={{
+        ...styles.viewStyle,
+        opacity: fadeAnim,
+        paddingHorizontal: 15,
+        top:
+          Platform.OS === 'ios'
+            ? toastMsg === ''
+              ? -100
+              : 50
+            : toastMsg === ''
+            ? -100
+            : 20,
+      }}>
+      <Text style={{color: Colors.white, fontFamily: Fonts.latoBold}}>
+        {toastMsg}
+      </Text>
+    </Animated.View>
+  );
 };
 
 const styles = StyleSheet.create({
-    viewStyle: {
-        position: 'absolute',
-        right: 0,
-        paddingVertical: 11,
-        width: Dimensions.get('window').width - 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: "#F33A6A",
-        alignSelf: 'center',
-        borderRadius: 15,
-        marginHorizontal: 15,
-    },
+  viewStyle: {
+    position: 'absolute',
+    right: 0,
+    paddingVertical: 11,
+    width: Dimensions.get('window').width - 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F33A6A',
+    alignSelf: 'center',
+    borderRadius: 15,
+    marginHorizontal: 15,
+  },
 });
 export default CustomToast;
